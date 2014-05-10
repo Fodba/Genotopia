b0e6fc7c84407b66ce2e0a159bfc789d4b7cfca3
Fef <ffodba@gmail.com>
2014-05-10 22:03:50 +0200

    Classe "BaseAzotee": Création des 5 types de base.

---
+++ CatalogueGene/CatalogueGene/BaseAzotee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CatalogueGene
+{
+    /// <summary>
+    /// type enumérable qui permet de distinguer chaque base azotée.
+    /// </summary>
+    public enum BaseADN { BaseA, BaseC, BaseG, BaseT, BaseU }
+    public class BaseAzotee
+    {
+        private Random r = new Random();
+        public BaseADN Type;
+        public string Nom;
+
+        public BaseAzotee(bool adn = true)
+        {
+            int n;
+            if (adn)
+            {
+                n = r.Next(4);
+            }
+            else
+            {
+                n = r.Next(5);
+            }
+            switch (n)
+            {
+                case 0:
+                    this.Type = BaseADN.BaseA;
+                    this.Nom = "Base A";
+                    break;
+                case 1:
+                    this.Type = BaseADN.BaseC;
+                    this.Nom = "Base C";
+                    break;
+                case 2:
+                    this.Type = BaseADN.BaseG;
+                    this.Nom = "Base G";
+                    break;
+                case 3:
+                    this.Type = BaseADN.BaseT;
+                    this.Nom = "Base T";
+                    break;
+                default:
+                    this.Type = BaseADN.BaseU;
+                    this.Nom = "Base U";
+                    break;
+            }
+        }
+    }
+}
