6e63d18484acc599577f916e31aafae0a74bedfd
Fef <ffodba@gmail.com>
2014-05-12 23:58:16 +0200

    Création de la bibliothèque de classes "CatalogueGene". Classe "BaseAzotee": Création des 5 types de base.
    Signed-off-by: Fef <ffodba@gmail.com>

---
+++ CatalogueGene/CatalogueGene/BaseAzotee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CatalogueGene
+{
+    /// <summary>
+    /// énumération qui identifie les types de Base existants.
+    /// </summary>
+    public enum BaseADN { BaseA, BaseC, BaseG, BaseT, BaseU }
+    public class BaseAzotee
+    {
+        public Random r = new Random();
+        public BaseADN Type;
+        public string Nom;
+
+        public BaseAzotee(int n = 4)
+        {
+            switch (r.Next(n))
+            {
+                case 0:
+                    this.Type = BaseADN.BaseA;
+                    this.Nom = "A";
+                    break;
+                case 1:
+                    this.Type = BaseADN.BaseC;
+                    this.Nom = "C";
+                    break;
+                case 2:
+                    this.Type = BaseADN.BaseG;
+                    this.Nom = "G";
+                    break;
+                case 3:
+                    this.Type = BaseADN.BaseT;
+                    this.Nom = "T";
+                    break;
+                default:
+                    this.Type = BaseADN.BaseU;
+                    this.Nom = "U";
+                    break;
+            }
+            /// arret du processus pour permettre la génération d'un autre nombre aléatoire.
+            Thread.Sleep(20);
+        }
+    }
+}
